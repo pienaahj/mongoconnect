567f7946751b59be349de679203fd4e923825258
Hendrik Pienaar <pienaahj@gmail.com>
2022-03-10 14:52:17 +0200

    Added exportable variables and deleted MoreItems

--- mongoconnect.go
+++ mongoconnect.go
@@ -3,7 +3,6 @@ package mongoconnect
 import (
 	"context"
 	"fmt"
-	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -14,9 +13,9 @@ import (
 
 // declare variables
 var (
-	database *mongo.Database
-	client *mongo.Client
-	collection *mongo.Collection
+	Database *mongo.Database
+	Client *mongo.Client
+	Collection *mongo.Collection
 
 )
 
@@ -29,15 +28,15 @@ func ConfigDB(dbName string, colName string) {
 	defer cancel()
 	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:myadminpassword@192.168.0.148:27017/" + dbName))
+	Client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://admin:myadminpassword@192.168.0.148:27017/" + dbName))
 	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
+		if err = Client.Disconnect(ctx); err != nil {
 			panic(err)
 		}
 	}()
 
-	database = client.Database(dbName)
-	collection = database.Collection(colName)
+	Database = Client.Database(dbName)
+	Collection = Database.Collection(colName)
 }
 
 
@@ -84,77 +83,52 @@ func CreateEntry(dbs *mongo.Database, collection string, doc bson.D) (interface{
 // id := res.InsertedID
 
 
-// Several query methods return a cursor, which can be used like this:
-// MoreItems returns more than one item from the database
-func MoreItems(dbs *mongo.Database, collection string) (interface{}, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	cur, err := dbs.Collection(collection).Find(ctx, bson.D{})
-	if err != nil { log.Fatal(err) }
-	defer cur.Close(ctx)
-	// make the results slice
-	type result struct{
-		Foo string
-		Bar int32
-	}
-	var results []result
-	for cur.Next(ctx) {
-	// To decode into a struct, use cursor.Decode()
-	result := struct{
-		Foo string
-		Bar int32
-	}{}
-	err := cur.Decode(&result)
-	if err != nil { log.Fatal(err) }
-	// do something with result...
-	results = append(results, result)
-	// To get the raw bson bytes use cursor.Current
-	// raw := cur.Current
-	// do something with raw...
-	}
-	if err := cur.Err(); err != nil {
-		return nil, err
-	}
-	return results, nil
-}
-
-
 // SingleItem returns a single item from the database
 // For methods that return a single item, a SingleResult, which works like a *sql.Row:
 // filter := bson.D{{"name", "pi"}}
-func SingleItem(filter bson.D) (interface{}, error) {
+func SingleItem(filter bson.D) (bson.D, error) {
 	// reserve momory for result
-	var result struct {
-    	Value float64
-	}
+	var result bson.D
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	err := collection.FindOne(ctx, filter).Decode(&result)
+	err := Collection.FindOne(ctx, filter).Decode(&result)
 	if err == mongo.ErrNoDocuments {
 		// Do something when no record was found
 		fmt.Println("record does not exist")
 		return nil, fmt.Errorf("could not find record : %q with error: %q", filter, err)
 	} else if err != nil {
-		log.Fatal(err)
 		return nil, fmt.Errorf("an error:%q occured while finding filter : %q", err, filter)
 	}
 	// Do something with result...
 
 	return result, nil
 }
-// DeleteEntry removes a item from the database
-func DeleteEntry(dbs *mongo.Database, colName string, filter bson.D) bool {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	// get the collection
-	coll := dbs.Collection(colName)
 
-	result, err := coll.DeleteOne(ctx, filter)
-	if err != nil {
-		fmt.Printf("an error occured while tryng to delete %q", filter)
-		return false
+// AllItems retrieves all items in a collection
+func AllItems(dbs *mongo.Database, colName string) ([]bson.D, error) {
+	// reserve momory for result
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	cur, err := dbs.Collection(colName).Find(ctx, bson.D{{}})
+	if err != nil { 
+		return nil, fmt.Errorf("an error:%q occured while finding all items", err)
 	}
-	fmt.Printf("record delete : %q", result)
-	return true
+	defer cur.Close(ctx)
+	var results []bson.D
+
+	// To decode into result, use cursor.All()
+	err = cur.All(ctx, &results)
+	if err != nil {
+		return nil, fmt.Errorf("an error:%q occured while decoding all items", err)
+	}
+
+	// To get the raw bson bytes use cursor.Current
+	// raw := cur.Current
+	// do something with raw...
+	
+	if err := cur.Err(); err != nil {
+		return nil, fmt.Errorf("an error:%q occured on cursor", err)
+	}
+	return results, nil
 }
