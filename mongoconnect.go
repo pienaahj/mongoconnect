0526b7997845d2696b1848ee55f9cbb672599e71
Hendrik Pienaar <pienaahj@gmail.com>
2022-03-17 10:19:01 +0200

    Added CreateEntries, RemoveOne and RemoveMany

--- mongoconnect.go
+++ mongoconnect.go
@@ -2,11 +2,13 @@ package mongoconnect
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
@@ -17,7 +19,15 @@ var (
 	Collection *mongo.Collection 
 )
 
-
+// Notes:
+// To insert a document into a collection, first retrieve a Database and then 
+// Collection instance from the Client:
+// collection := client.Database("testdb").Collection("numbers")
+// The Collection instance can then be used to insert documents:
+// ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+// defer cancel()
+// res, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
+// id := res.InsertedID
 
 // ConfigDB populates the database variables by connecting to a
 // mongo database(dbName) and collection(colName)with a 
@@ -27,7 +37,7 @@ var (
 
 // "mongodb://admin:myadminpassword@192.168.0.148:27017/dbName"
 
-// This needs to be in the main.go file 
+// This needs to be in the main.go file as a client expires with the main function
 // func ConfigDB(conStr string, dbName string, colName string) {
 // 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 // 	defer cancel()
@@ -59,17 +69,11 @@ func CheckConnection(client *mongo.Client) bool {
 	return true
 }
 
-
-// To insert a document into a collection, first retrieve a Database and then 
-// Collection instance from the Client:
-
-// collection := client.Database("testdb").Collection("numbers")
-
 //  CreateEntry adds a record(doc) to the database(dbs) into Collection(collection)
 func CreateEntry(dbs *mongo.Database, collection string, doc bson.D) (interface{}, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	// gert the database name
+	// get the database name
 	dbName := dbs.Name()
 	// res, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
 	res, err := dbs.Collection(collection).InsertOne(ctx, doc)
@@ -80,13 +84,21 @@ func CreateEntry(dbs *mongo.Database, collection string, doc bson.D) (interface{
 	return id, nil
 }
 
-
-// The Collection instance can then be used to insert documents:
-// ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
-// defer cancel()
-// res, err := collection.InsertOne(ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
-// id := res.InsertedID
-
+//  CreateEntries adds a records(docs) to the database(dbs) into Collection(collection)
+func CreateEntries(dbs *mongo.Database, collection string, docs []interface{}) ([]interface{}, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	// get the database name
+	dbName := dbs.Name()
+	// Set the order option to false to allow operations to happen even if one of them errors
+	opts := options.InsertMany().SetOrdered(false)
+	res, err := dbs.Collection(collection).InsertMany(ctx, docs, opts)
+	ids := res.InsertedIDs
+	if err != nil {
+		return nil, fmt.Errorf("could not create record into : %s with error: %q",dbName, err)
+	}
+	return ids, nil
+}
 
 // SingleItem returns a single item from the database
 // For methods that return a single item, a SingleResult, which works like a *sql.Row:
@@ -136,4 +148,40 @@ func AllItems(dbs *mongo.Database, colName string) ([]bson.M, error) {
 		return nil, fmt.Errorf("an error:%q occured on cursor", err)
 	}
 	return results, nil
+}
+
+// RemoveOne deletes a record from a collection
+func RemoveOne(collection *mongo.Collection, filter interface{}) (*mongo.DeleteResult, error) {
+	// create an expiring context
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	// set options to eliminate case sensitivity ie."name" filed is "Bob" or "bob"
+	opts := options.Delete().SetCollation(&options.Collation{
+		Locale: "en_US",
+		Strength: 1,
+		CaseLevel: false,
+	})
+	res, err := collection.DeleteOne(ctx, filter, opts)
+	if err != nil {
+		return nil, errors.New("could not delete record from mongodb ")
+	}
+	return res, nil
+}
+
+// RemoveMany deletes multiple record from a collection
+func RemoveMany(collection *mongo.Collection, filter interface{}) (*mongo.DeleteResult, error) {
+	// create an expiring context
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	// set options to eliminate case sensitivity ie."name" filed is "Bob" or "bob"
+	opts := options.Delete().SetCollation(&options.Collation{
+		Locale: "en_US",
+		Strength: 1,
+		CaseLevel: false,
+	})
+	res, err := collection.DeleteMany(ctx, filter, opts)
+	if err != nil {
+		return nil, errors.New("could not delete record from mongodb ")
+	}
+	return res, nil
 }
